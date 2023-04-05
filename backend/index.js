fc66f586b469514dbcc2b4aa77a2d4adc28e0b66
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-04-05 19:26:57 +0530

    Request bug solved from the request.rest file

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/authenticate", async (res, req) => {
+app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
   return res.json({ username: username, secret: "sha256..." });
 });
