dd8565d72c8e1d620310245f3303dbe7a1ae588c
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-04-18 01:06:21 +0530

    Added New Data

--- voiceAssistant.py
+++ voiceAssistant.py
@@ -1,2 +1,19 @@
 import pyttsx3
-import speech_recognition
+import speech_recognition as sr
+import webbrowser
+import datetime
+import pyjokes 
+
+
+def sptext():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening Broda...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+        try:
+            print("Recognizing your Voice ....")
+            data = recognizer.recognizer_google(audio)
+            print(data)
+        except Exception as e:
+            raise e
