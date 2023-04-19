81e8ef9e5a02f793a789868ea9a8f32ca88f414a
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-04-20 01:08:38 +0530

    Added Activating system

---
+++ dost_main.py
@@ -0,0 +1,28 @@
+import pyttsx3 
+import speech_recognition
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+engine.setProperty("rate", 170)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takeCommand():
+    r = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Listening Sir")
+        r.pause_threshold = 1
+        r.energy_threshold = 300
+        audio = r.listen(source, 0, 5)
+    try:
+        print("Understanding..")
+        query = r.recognize_google(audio, language = 'en-in')
+        print(f"You said : {query}")
+    except Exception as e :
+        print("Say that again")
+        return "None"
+    return query
