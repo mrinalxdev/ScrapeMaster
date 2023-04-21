0e35d09cf39e1e132c603fd85c00875ca5034b52
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-04-22 02:40:23 +0530

    Search on web ability

--- SearchNow.py
+++ SearchNow.py
@@ -2,6 +2,7 @@ import speech_recognition
 import pyttsx3
 import pywhatkit
 import wikipedia
+import webbrowser
 
 def takeCommand():
     r = speech_recognition.Recognizer()
@@ -33,4 +34,33 @@ def speak(audio):
 
 def searchGoogle(query):
     if "google" in query:
-        import 
+        import wikipedia as googleScrap
+        query = query.replace("dost", "")
+        query = query.replace("google search", "")
+        query = query.replace("google", "")
+        speak ("This what i got gor your search")
+
+        try : 
+            pywhatkit.search(query)
+            result = googleScrap.summary(query,1)
+            speak(result)
+        except :
+            speak("No speakable output")
+        
+
+def searchYoutube(query):
+    if "youtube" in query:
+        speak("This is what I found for your answer")
+        query = query.replace("youtube search", "")
+        query = query.replace("youtube", "")
+        query = query.replace("dost", "")
+        web = "https://www.youtube.com/results?search_query=" + query
+        webbrowser.open(web)
+        pywhatkit(query)
+        speak("Done, sir")
+
+def searchWiki(query):
+    if "wikipedia" in query:
+        speak("Searching from wikipedia....")
+
+    
