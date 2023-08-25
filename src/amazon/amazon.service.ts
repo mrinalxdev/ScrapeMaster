0d988c15eb6af2af22750ce74265e3a4595dd2da
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-08-26 04:42:19 +0530

    Final Commit

--- src/amazon/amazon.service.ts
+++ src/amazon/amazon.service.ts
@@ -9,20 +9,35 @@ export class AmazonService {
     const browser = await puppeteer.connect({
       browserWSEndpoint: this.configService.getOrThrow("SBR_WS_ENDPOINT"),
     });
-    try{
-        const page = await browser.newPage()
-        page.setDefaultNavigationTimeout(2 * 60 * 1000)
+    try {
+      const page = await browser.newPage();
+      page.setDefaultNavigationTimeout(2 * 60 * 1000);
+      await Promise.all([
+        page.waitForNavigation(),
+        page.goto("https://amazon.com"),
+      ]);
+      await page.type("#twotabsearchtextbox", products),
         await Promise.all([
-            page.waitForNavigation(),
-            page.goto('https://amazon.com')
-        ])
-        await page.type('#twotabsearchtextbox', products),
-        await Promise.all([
-            page.waitForNavigation(),
-            page.click('#nav-search-submit-button')
-        ])
-    }finally {
-        await browser.close();
+          page.waitForNavigation(),
+          page.click("#nav-search-submit-button"),
+        ]);
+      return await page.$$eval(
+        ".s-search-results .s-card-container",
+        (resultItems) => {
+          return resultItems.map((resultItem) => {
+            const url = resultItem.querySelector("a").href;
+            const title = resultItem.querySelector(
+              ".s-title-instructions-style"
+            )?.textContent;
+            const price = resultItem.querySelector('.a-price .a-offscreen')?.textContent
+            return {
+                url, title, price
+            }
+          });
+        }
+      );
+    } finally {
+      await browser.close();
     }
   }
 }
